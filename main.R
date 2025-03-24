dfc4ca41f598087061c5cbbbd9ed12f3538ea35d
Lineax17 <philipp.hartmann04@gmail.com>
2025-03-24 17:07:16 +0100

    read data as factors

--- main.R
+++ main.R
@@ -1,3 +1,11 @@
 data <- read.csv("./data/heart.csv",header=TRUE,sep=",",fill=TRUE,stringsAsFactors=TRUE)
 head(data)
 
+data$Sex <- as.factor(data$Sex)
+data$ChestPainType <- as.factor(data$ChestPainType)
+data$FastingBS <- as.factor(data$FastingBS)
+data$RestingECG <- as.factor(data$RestingECG)
+data$ExerciseAngina <- as.factor(data$ExerciseAngina)
+data$ST_Slope <- as.factor(data$ST_Slope)
+data$HeartDisease <- as.factor(data$HeartDisease) #Zielvariable
+
