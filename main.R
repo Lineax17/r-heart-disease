06e62d3c07298ad5b2de078287c6bf4111b70784
Lineax17 <philipp.hartmann04@gmail.com>
2025-05-06 12:44:24 +0200

    add visualization of factorial data

--- main.R
+++ main.R
@@ -1,4 +1,7 @@
-# read data
+#######################################
+# Read data & Set correct types
+#######################################
+
 data <- read.csv("./data/heart.csv",header=TRUE,sep=",",fill=TRUE,stringsAsFactors=TRUE)
 head(data)
 
@@ -13,6 +16,10 @@ data$HeartDisease <- as.factor(data$HeartDisease) #target
 summary(data)
 plot(data)
 
+#######################################
+# Visulization
+#######################################
+
 # visualize numeric data
 par(mfrow=c(1,2))
 boxplot(data$Age, main = "Age")
@@ -22,15 +29,93 @@ par(mfrow=c(1,2))
 boxplot(data$Cholesterol, main = "Cholesterol")
 boxplot(data$MaxHR, main = "MaxHR")
 
-par(mfrow=c(1,2))
 boxplot(data$Age ~ data$HeartDisease, ylab = "Age", xlab = "Disease")
 boxplot(data$RestingBP ~ data$HeartDisease, ylab = "RestingBP", xlab = "Disease")
 
-par(mfrow=c(1,2))
 boxplot(data$Cholesterol ~ data$HeartDisease, ylab = "Cholesterol", xlab = "Disease")
 boxplot(data$MaxHR ~ data$HeartDisease, ylab = "MaxHR", xlab = "Disease")
 
-# splitting data into test and train
+boxplot(data$Oldpeak ~ data$HeartDisease, ylab="Oldpeak", xlab = "Disease")
+
+# visualize factorial data
+tab <- table(data$Sex, data$HeartDisease)
+prop <- prop.table(tab, margin = 1)
+barplot(t(prop),
+        beside = TRUE,
+        legend.text = TRUE,
+        args.legend = list(x = "topright"),
+        main = "Disease by Gender",
+        xlab = "Gender",
+        ylab = "",
+        ylim = c(0,1),
+        col  = c("darkgreen", "red"))
+
+tab <- table(data$ChestPainType, data$HeartDisease)
+prop <- prop.table(tab, margin = 1)
+barplot(t(prop),
+        beside = TRUE,
+        legend.text = TRUE,
+        args.legend = list(x = "topright"),
+        main = "Disease by ChestPainType",
+        xlab = "ChestPainType",
+        ylab = "",
+        ylim = c(0,1),
+        col = c("darkgreen", "red"))
+
+tab <- table(data$FastingBS, data$HeartDisease)
+prop <- prop.table(tab, margin = 1)
+barplot(t(prop),
+        beside = TRUE,
+        legend.text = TRUE,
+        args.legend = list(x = "topright"),
+        main = "Disease by FastingBS",
+        xlab = "FastingBS",
+        ylab = "",
+        ylim = c(0,1),
+        col = c("darkgreen", "red"),
+        names.arg = c("no", "yes"))
+
+tab <- table(data$RestingECG, data$HeartDisease)
+prop <- prop.table(tab, margin = 1)
+barplot(t(prop),
+        beside = TRUE,
+        legend.text = TRUE,
+        args.legend = list(x = "topright"),
+        main = "Disease by RestingECG",
+        xlab = "RestingECG",
+        ylab = "",
+        ylim = c(0,1),
+        col = c("darkgreen", "red"))
+
+tab <- table(data$ExerciseAngina, data$HeartDisease)
+prop <- prop.table(tab, margin = 1)
+barplot(t(prop),
+        beside = TRUE,
+        legend.text = TRUE,
+        args.legend = list(x = "topright"),
+        main = "Disease by ExerciseAngina",
+        xlab = "ExerciseAngina",
+        ylab = "",
+        ylim = c(0,1),
+        col = c("darkgreen", "red"))
+
+
+tab <- table(data$ST_Slope, data$HeartDisease)
+prop <- prop.table(tab, margin = 1)
+barplot(t(prop),
+        beside = TRUE,
+        legend.text = TRUE,
+        args.legend = list(x = "topright"),
+        main = "Disease by ST_Slope",
+        xlab = "ST_Slope",
+        ylab = "",
+        ylim = c(0,1),
+        col = c("darkgreen", "red"))
+
+#######################################
+# Split in training and test data
+#######################################
+
 library(caret)
 set.seed(467)
 trainIndex <- createDataPartition(data$HeartDisease, p = 0.8, list = FALSE)
@@ -39,7 +124,10 @@ testData <- data[-trainIndex, ]
 table(trainData$HeartDisease)
 table(testData$HeartDisease)
 
-# train logistic regression
+#######################################
+# Logistic Regression
+#######################################
+
 log_reg_model <- glm(HeartDisease ~ ., data = trainData, family = "binomial")
 log_reg_prob <- predict(log_reg_model, newdata = testData, type="response")
 log_reg_pred <- ifelse(log_reg_prob > 0.5, 1, 0)
@@ -50,7 +138,10 @@ confusion <- table(Predicted = log_reg_pred, Actual = actual)
 cat("Logistic Regression Confusion Matrix:\n")
 print(confusion)
 
-# train decision tree
+#######################################
+# Decision Tree
+#######################################
+
 library(rpart)
 tree_model <- rpart(HeartDisease ~ ., data = trainData, method = "class")
 tree_pred <- predict(tree_model, newdata = testData, type = "class")
@@ -60,3 +151,7 @@ tree_probs <- predict(tree_model, newdata = testData, type = "prob")[,2]
 actual <- as.numeric(as.character(testData$HeartDisease))
 cat("Decision Tree Confusion Matrix:\n")
 print(table(Predicted = tree_pred, Actual = actual))
+
+#######################################
+# Random Forest
+#######################################
